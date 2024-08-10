99b95330d8361ac7f1e6588a274f1f359aa36a4e
Basith-m <abm2k20@gmail.com>
2024-08-10 20:43:43 +0530

    BaseUrl updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5257',
+    baseURL: 'https://media-player-backed.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
